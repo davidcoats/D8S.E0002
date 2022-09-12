5555ec1f1a86b84ba7b57a13898c9bcc5d817614
David Coats <david@getsafetycone.com>
2022-09-12 11:04:06 -0700

    Initial commit.

---
+++ source/D8S.E0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.E0002
+{
+	/// <summary>
+	/// Repository for blog post on creating service providers from scratch.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/D8S.E0002/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace D8S.E0002
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
